a890286603e77bcc05f5f62c285ff01615d6ba44
ayomide <omoniyiayomide987@gmail.com>
2024-04-16 02:28:58 +0100

    first commit

--- src/main/java/com/example/contactmanagement/data/repository/ContactRepository.java
+++ src/main/java/com/example/contactmanagement/data/repository/ContactRepository.java
@@ -1,11 +1,11 @@
-package africaSemicolon.contact4.data.repository;
+package africaSemicolon.data.repository;
 
-import africaSemicolon.contact4.data.model.Contact;
+import africaSemicolon.data.model.Contact;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
 
 public interface ContactRepository extends MongoRepository<Contact, String> {
     Contact findContact(String username);
-
-    Contact findByFirstName(String firstName);
 
 }
--- src/main/java/com/example/contactmanagement/data/repository/UserRepository.java
+++ src/main/java/com/example/contactmanagement/data/repository/UserRepository.java
@@ -1,8 +1,9 @@
-package africaSemicolon.contact4.data.repository;
+package africaSemicolon.data.repository;
 
-import africaSemicolon.contact4.data.model.User;
+import africaSemicolon.data.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface UserRepository extends MongoRepository<User, String> {
     User findByUsername(String username);
+
 }
